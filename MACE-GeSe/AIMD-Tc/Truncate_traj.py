385d4c12a8db3ab20e21c053f1e8e37586385878
Tina Mihm <tmihm@bu.edu>
2025-08-20 22:12:40 -0400

    AIMD-MACE-Tc

--- MACE-GeSe/AIMD-Tc/Truncate_traj.py
+++
@@ -1,8 +0,0 @@
-from ase.io import read, write
-from glob import glob
-import re
-
-for traj_name in glob('Test_Temp*/traj/md.traj'):
-    traj = read(traj_name,'::200')
-    T = int(re.findall(r'\d+', traj_name)[0]) #now updated
-    write(f'Trunc_Traj/MD_{T:d}K.traj', traj)
